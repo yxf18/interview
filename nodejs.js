de26eb948db5bd3f00528c82e2250365c28dc376
yangxuefeng <yangxuefeng@clinbrain.com>
2024-03-06 16:16:21 +0800

    fix: 完善

---
+++ nodejs.js
@@ -0,0 +1,26 @@
+// 寻找差集
+const fs = require("fs");
+const path = require("path");
+const _ = require("lodash");
+
+// 指定要读取的目录路径
+const directoryPath = "./everyDay";
+
+// 使用 fs.readdir() 读取目录
+fs.readdir(directoryPath, (err, files) => {
+  if (err) {
+    console.error("Error reading directory:", err);
+    return;
+  }
+  const arrSet = new Set([]);
+  files.forEach((file) => {
+    arrSet.add(+file.split(".")[0]);
+  });
+
+  const fillArr = _.range(1, 901);
+  const diffArr = _.difference(
+    fillArr,
+    [...arrSet].sort((a, b) => a - b)
+  );
+  console.log("[ diffArr ]", diffArr);
+});
